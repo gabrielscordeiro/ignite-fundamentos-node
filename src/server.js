a336b96d79b9e901f639457f7ff39bb87f5e3eba
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-03-06 23:01:14 -0300

    feat: 🎸 Application routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
+    const { method, url } = req
+
+    if (method === 'GET' && url === '/users') {
+        return res.end('Users list')
+    }
+
+    if (method === 'POST' && url === '/users') {
+        return res.end('Create user')
+    }
+
     return res.end('Hello World')
 })
 
-server.listen(3333) 
+server.listen(3333)
